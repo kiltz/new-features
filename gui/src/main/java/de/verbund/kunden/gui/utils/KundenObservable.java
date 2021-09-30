7a0b9f03c5907094414fbeb78842b3f44667aafc
Friedrich Kiltz <f@kiltz.de>
2021-09-30 13:51:49 +0200

    historische Ausgangs-App

---
+++ gui/src/main/java/de/verbund/kunden/gui/utils/KundenObservable.java
@@ -0,0 +1,10 @@
+package de.verbund.kunden.gui.utils;
+
+import java.util.Observable;
+
+public class KundenObservable extends Observable {
+
+    @Override public void setChanged() {
+        super.setChanged();
+    }
+}
