b823730d4309f74f35b4de726ad9c05106a46e0e
Friedrich Kiltz <f@kiltz.de>
2021-09-30 15:57:49 +0200

    build.gradle angepasst

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # new-features
+
+Beispieldateien aus dem Buch
+
+Michael Inden: Java – die Neuerungen
+in Version 9 bis 14
+
+und aus anderen Tutorials
--- src/main/java/de/kiltz/neu/in8/DasOptionale.java
+++ src/main/java/de/kiltz/neu/in8/DasOptionale.java
@@ -8,10 +8,60 @@ public class DasOptionale {
         erzeugen();
         behandeln();
         primitiveDatenTypen();
+        mitLambdaUndMethodenReferenz();
+        mappenUndFiltern();
+
+    }
+
+    private static void mappenUndFiltern() {
+        String x = wuerfle() == 6 ? null : "abcdef";
+        // Filtern: alt
+        if (x != null && x.contains("ab")) {
+            System.out.println(x);
+        }
+        // Filtern: neu
+        var opt = Optional.ofNullable(x);
+        opt.filter(str -> str.contains("ab")).ifPresent(System.out::println);
+
+        // Mappen und filtern: alt
+        if (x != null) {
+            String str = x.toUpperCase();
+            if (str.contains("AB")) {
+                System.out.println(str);
+            }
+        }
+        // Mappen und filtern: neu
+        opt.map((String::toUpperCase)).
+                filter(s -> s.contains("AB")).
+                ifPresent(System.out::println);
+    }
+
+    private static int wuerfle() {
+        int zahl = new Random().nextInt(6) + 1;
+        System.out.println("Würfel: " + zahl);
+
+        return zahl;
+    }
+
+    private static void mitLambdaUndMethodenReferenz() {
+        Optional<String> da = Optional.of("Es ist da");
+        Optional<String> nada = Optional.ofNullable(null);
+        Optional<String> auchNada = Optional.empty();
+
+        var obj = new DasOptionale();
+        da.ifPresent(s -> obj.print(s));
+        da.ifPresent(obj::print);
+        nada.ifPresent(obj::print);
+        auchNada.ifPresent(obj::print);
+
+    }
+
+    private void print(String s) {
+        System.out.println("Print: " + s);
     }
 
     private static void primitiveDatenTypen() {
-        final int[] sampleValues = {1,3,5,7,11,13,17,19};
+        final int[] sampleValues = {1, 3, 5, 7, 11, 13, 17, 19};
         // Berechnungen auf Streams mit primitiven Werten
         final OptionalInt min = Arrays.stream(sampleValues).min();
         final OptionalInt max = Arrays.stream(sampleValues).max();
@@ -28,9 +78,9 @@ public class DasOptionale {
         // Führe Aktion aus, wenn vorhanden
         min.ifPresent(System.out::println);
         // Alternativen Wert liefern, wenn nicht vorhanden
-        System.out.println(min.orElse( 1));
+        System.out.println(min.orElse(1));
         // Berechne Ersatzwert, wenn nicht vorhanden
-        final Supplier<Integer> randomGenerator = () -> (int)(100 * Math.random());
+        final Supplier<Integer> randomGenerator = () -> (int) (100 * Math.random());
         System.out.println(min.orElseGet(randomGenerator));
         // Löse eine Exception aus, wenn nicht vorhanden
         //min.orElseThrow(() -> new NoSuchElementException("there is no minimum"));
@@ -43,7 +93,7 @@ public class DasOptionale {
         final Comparator<Integer> naturalOrder = Comparator.naturalOrder();
 
         final Optional<Integer> max = Arrays.stream(sampleValues).max(naturalOrder);
-        final Optional<Integer> min =Arrays.stream(noValues).min(naturalOrder);
+        final Optional<Integer> min = Arrays.stream(noValues).min(naturalOrder);
 
         System.out.println("max: " + max);
         System.out.println("min: " + min);
@@ -52,7 +102,7 @@ public class DasOptionale {
         System.out.println("isPresent?: " + min.isPresent());
         // Zugriff auf den Wert
         final Integer maxValue = max.get();
-        System.out.println ("maxValue: " + maxValue);
+        System.out.println("maxValue: " + maxValue);
 
         // Konstruktionsmethoden
         final Optional<Integer> optionalFromValue = Optional.of(4711);
