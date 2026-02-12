bb5fbdf33c2073f4abef990918ce7718b7b9369d
Friedrich Kiltz <friedrich.kiltz@ofd.bwl.de>
2026-02-12 12:32:47 +0100

    Gradle Update

--- src/main/java/de/kiltz/neu/in13und14/EinRecord.java
+++ src/main/java/de/kiltz/neu/in13und14/EinRecord.java
@@ -1,4 +1,4 @@
-package de.finkonsens.cv.sandkasten.neu.in13und14;
+package de.kiltz.neu.in13und14;
 
 public record EinRecord(String vorname, String nachname, int alter) {
     public String name() {
